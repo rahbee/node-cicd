2d98b4e3885cc4bf37963c37ffa6ed4cb71f0b0a
Rahbee Alvee <ralvee@scentregroup.com>
2024-12-04 18:56:04 +1100

    fix deployment script

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -2,5 +2,6 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 heroku container:login
+heroku stack:set container --app rahbee-distnode
 heroku container:push web --app rahbee-distnode
 heroku container:release web --app rahbee-distnode
